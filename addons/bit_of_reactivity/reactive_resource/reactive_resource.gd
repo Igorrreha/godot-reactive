3c44a632698fb57efa5c09583334166ebab71222
igorecha.9999 <igorecha.9999@gmail.com>
2023-07-04 19:30:43 +0700

    Add a short way to create bindings

--- addons/bit_of_reactivity/reactive_resource/reactive_resource.gd
+++ addons/bit_of_reactivity/reactive_resource/reactive_resource.gd
@@ -12,6 +12,31 @@ extends Resource
 var _bindings: Dictionary#[StringName, Array[ReactiveResource.Binding]]
 
 
+## "Syntactic sugar" for complex setuping of an object.
+## This method
+## 1. removes old bindings if necessary
+## 2. creates new bindings
+## 3. sets resource_property value to self
+## 4. calls the callbacks of the created bindings
+func setup_bindings(resource_holder: Object, resource_property: String,
+		bindings: Array[Binding]) -> void:
+	if not resource_property in resource_holder:
+		printerr("Binding failed! %s is not in %s" % [resource_property, resource_holder])
+		return
+	
+	var old_resource: ReactiveResource = resource_holder[resource_property]
+	for binding in bindings:
+		if old_resource:
+			old_resource.remove_callback(binding.property, binding.callback)
+		
+		self.bind(binding.property, binding.callback, binding.caller)
+	
+	resource_holder[resource_property] = self
+	
+	for binding in bindings:
+		binding.callback.call()
+
+
 ## Setter for reactive property of resource.
 ## After applying the value invokes all binded callables.
 ## The caller argument is used to ignore some bindings invoking
@@ -34,7 +59,7 @@ func bind(property: StringName, callback: Callable, caller: Callable = func():) 
 		var bindings: Array[Binding]
 		_bindings[property] = bindings
 	
-	_bindings[property].append(Binding.new(callback, caller))
+	_bindings[property].append(Binding.new(property, callback, caller))
 
 
 ## Removes all [ReactiveResource.Binding]s of property that contains provided callback
@@ -47,18 +72,20 @@ func remove_callback(property: StringName, callback: Callable) -> void:
 
 
 class Binding:
+	var property: String
 	## The method that invokes at binding applying
 	var callback: Callable
 	## The caller is used for evading of resetting property by callback in the place where it
 	## already setted manually
-	var _caller: Callable
+	var caller: Callable
 	
 	
-	func _init(callback: Callable, caller: Callable) -> void:
+	func _init(property: String, callback: Callable, caller: Callable = func():) -> void:
 		self.callback = callback
-		_caller = caller
+		self.property = property
+		self.caller = caller
 	
 	
-	func apply(caller: Callable) -> void:
-		if caller != _caller:
+	func apply(caller: Callable = func():) -> void:
+		if self.caller != caller:
 			callback.call()
