747c04ddf94be311d776e814331abb454326716d
igorecha.9999 <igorecha.9999@gmail.com>
2023-07-07 14:18:09 +0700

    Update reactive collection demo

--- addons/bit_of_reactivity/reactive_resource/demo/view/reactive_collection_container.gd
+++ addons/bit_of_reactivity/reactive_resource/demo/view/reactive_collection_container.gd
@@ -1,56 +1,19 @@
 class_name DemoReactiveCollectionContainer
-extends VBoxContainer
+extends ReactiveCollectionContainer
 
 
-@export var _collection_item_tscn: PackedScene
-@export var _items_container: Node
-
-var _state_handler: ReactiveResource.PropertyHandler
-var _state: Array[String]
-
-
-func setup(collection_handler: ReactiveResource.PropertyHandler) -> void:
-	if _state_handler:
-		_state_handler.remove_callback(_on_state_updated)
-	
-	_state_handler = collection_handler
-	_state_handler.bind(_on_state_updated, _update_state)
-	
-	_on_state_updated()
+# This script demonstrates the simplest way of creating a reactive collection container.
+# The main part of logic is in ReactiveCollectionContainer template class, if you want to get more
+# detalized view - please dive inside it
 
 
 func add_item() -> void:
 	var new_item = str(randi())
-	_state.append(new_item)
-	_create_item_node(new_item)
-	_update_state()
+	_append_state_item(new_item)
 
 
 func remove_last_item() -> void:
 	if _state.is_empty():
 		return
 	
-	var last_child_idx = _state.size() - 1
-	_state.remove_at(last_child_idx)
-	_items_container.remove_child(_items_container.get_child(last_child_idx))
-	_update_state()
-
-
-func _update_state() -> void:
-	_state_handler.set_value(_state, _update_state)
-
-
-func _on_state_updated() -> void:
-	_state = _state_handler.get_value()
-	
-	for child in _items_container.get_children():
-		child.queue_free()
-	
-	for item in _state:
-		_create_item_node(item)
-
-
-func _create_item_node(item) -> void:
-	var item_node = _collection_item_tscn.instantiate() as Label
-	item_node.text = item
-	_items_container.add_child(item_node)
+	_remove_state_item(_state[-1])
